eae99785682a516d82084c4e8263b01d238f8f70
mik-rzo <michael.panong3@gmail.com>
2023-05-30 16:13:44 +0100

    implemented feature to view list of reviews

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://house-of-games-7nlp.onrender.com/api'
+})
+
+export function getReviews() {
+    return api.get('/reviews').then((res) => {
+        return res.data
+    })
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from 'react'
+import { getReviews } from '../api.js'
+import { ReviewCard } from './ReviewCard.jsx'
+
 export function Home() {
-  return <h2>Home page</h2>
+  const [reviews, setReviews] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    getReviews().then((data) => {
+      setReviews(data.reviews)
+    })
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    )
+  }
+
+  return (
+    <main>
+      <h2>Home page</h2>
+      <ul>
+        {reviews.map((review) => {
+          return (
+            <li key={review.review_id} className='review-card'>
+              <ReviewCard review={review} />
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
 }
