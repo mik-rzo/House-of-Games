35b77ba7b077c0aa2d7088f94a206dbc7ab7aca8
mik-rzo <michael.panong3@gmail.com>
2023-05-31 12:09:26 +0100

    deleted code comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -7,8 +7,3 @@ export function CommentCard({ comment }) {
     </article>
   )
 }
-
-//           "body": "Now this is a story all about how, board games turned my life upside down",
-//           "author": "mallionaire",
-//           "votes": 13,
-//           "created_at": "2021-01-18T10:24:05.410Z"
